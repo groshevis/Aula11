33aaba072f1acf050b760e68474a6cab2c72dc55
groshevis <groshevis_eloisa@outook.com>
2019-03-18 12:15:49 -0300

    segunda_versão

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,4 +7,32 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+
+      valor1 : number = 0;
+      valor2 : number = 0;
+      valor3 : number = 0;
+
+
+  somar():void{
+
+    let resul1= Number(this.valor1) ;
+    let result2=Number(this.valor2) ;
+    let result3=Number(this.valor3);
+
+    if(resul1>result2>result3){
+      alert("O número " +this.valor1+ "é o maior")
+    }
+    else if(result2>resul1>result3){
+        alert("O numero " + this.valor2+" é o maior")
+    }
+    else
+    {
+        alert("O numero "+ this.valor3 +" é o maior")
+    }
+
+
+
+
+  }
+
 }
